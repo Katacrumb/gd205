bdbfcb5c27976e08e5513c8f4eeb11567af13281
Katacrumb <Baltodano358@gmail.com>
2023-10-16 22:17:26 -0400

    target click

---
+++ gd205/Assets/scripts/Cyclops.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cyclops : MonoBehaviour
+{
+
+    public float boomBoom = 5f;
+    public float boomBlast = 6f;
+    public GameObject prefab;
+
+
+    void FixedUpdate()
+    {
+        Ray laser = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit = new RaycastHit();
+
+        if(Physics.Raycast(laser, out hit)){
+
+            //Debug.Log("you hit a ball:" + hit.transform.name);
+            if(Input.GetMouseButton(0) && hit.rigidbody ){
+
+                hit.rigidbody.AddExplosionForce(boomBoom, hit.point, boomBlast);
+                   
+
+            }
+
+        }
+
+        if(Input.GetMouseButtonDown(1)){
+               
+          Instantiate(prefab,hit.point, Quaternion.identity);
+        }
+
+
+            
+    }
+
+} 
