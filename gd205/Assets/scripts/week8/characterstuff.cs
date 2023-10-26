e237efbe869f5bd1eb776e23baa0b72d9b2ee9c1
Katacrumb <143760368+Katacrumb@users.noreply.github.com>
2023-10-26 16:27:18 -0400

    animator (update)

--- gd205/Assets/scripts/week8/characterstuff.cs
+++ gd205/Assets/scripts/week8/characterstuff.cs
@@ -20,23 +20,40 @@ public class characterstuff : MonoBehaviour
 
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-        Debug.Log(move);
+        //Debug.Log(move);
 
         anim.SetFloat("Movement", move.z);
         controller.Move(move * speed * Time.deltaTime);
 
-      /*  if (move.z > 0f){
-            anim.SetInteger("Movement", 1);
-            
 
+        if (Input.GetKey(KeyCode.LeftShift)){
+            anim.SetBool("isRunning", true);
+            controller.Move(move * speed * Time.deltaTime);
+
+
+        } else {
+        anim.SetBool("isRunning",false);
+    
         }
-        */
+        //this shit does not work im clueless on why it doesnt
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.LeftShift)){
+           anim.SetFloat("isRunningBackwards", move.z);
+           controller.Move(move * speed * Time.deltaTime);
 
+            }
 
-
+        if(Input.GetKey(KeyCode.R)){
+            anim.SetBool("Punch",true);
+        }else{
+            anim.SetBool("Punch",false);
+        }
     }
+      
+      
+      
 }
+
+
